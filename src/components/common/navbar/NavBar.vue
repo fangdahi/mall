560e1efa6e6081f3fa5c8882cf13a2392b5f5bc1
dingqiaolong <1534291246@qq.com>
2022-03-04 15:18:34 +0800

    首页添加轮播图

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -13,8 +13,8 @@ export default {};
 <style>
 .nav-bar {
   display: flex;
-  height: 49px;
-  line-height: 49px;
+  height: 44px;
+  line-height: 44px;
   text-align: center;
   box-shadow: 0 1px 1px rgba(100, 100, 100, 0.2);
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,38 @@
 <template>
-  <nav-bar id="home">
+  <nav-bar id="home-nav">
     <template v-slot:nav-bar-center> 购物街 </template>
   </nav-bar>
+  <home-swiper :banners="banners"></home-swiper>
 </template>
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childComs/HomeSwiper.vue";
+import { getHomeMultidata } from "../../components/network/home";
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
   components: {
     NavBar,
+    HomeSwiper,
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   },
 };
 </script>
 
 <style scoped>
-#home {
+#home-nav {
   color: #fff;
   background-color: var(--color-tint);
 }
