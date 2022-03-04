f8145ee73258e7e3988b15c22da870c6812f12c7
dingqiaolong <1534291246@qq.com>
2022-03-04 10:43:48 +0800

    增加首页导航栏

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="nav-bar-left"></slot></div>
+    <div class="center"><slot name="nav-bar-center"></slot></div>
+    <div class="right"><slot name="nav-bar-right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  height: 49px;
+  line-height: 49px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.2);
+}
+
+.left,
+.right {
+  width: 60px;
+}
+
+.center {
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <h2>首页</h2>
+  <nav-bar id="home">
+    <template v-slot:nav-bar-center> 购物街 </template>
+  </nav-bar>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+import NavBar from "../../components/common/navbar/NavBar.vue";
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style scoped>
-
+#home {
+  color: #fff;
+  background-color: var(--color-tint);
+}
 </style>
